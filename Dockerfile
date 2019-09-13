5ac839825d8127b6c67df2841af51748319c4043
Kohlmann András <metuoku@outlook.com>
2019-09-13 05:14:20 +0200

    data driven tests

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:3
 
+RUN pip3 install ddt
 COPY . .
 
 ENTRYPOINT python -m unittest
--- test_duedate.py
+++ test_duedate.py
@@ -1,32 +1,37 @@
 import unittest
-
+from ddt import ddt, data, unpack
 from datetime import datetime
 from duedate import calculate_due_date
 
-
+@ddt
 class TestDueDateCalculator(unittest.TestCase):
-    def test_method_exists(self):
-        calculate_due_date(datetime.now(), 0)
-
-    def test_turnaround_time_zero_due_date_is_submit_time(self):
-        submit_time = datetime(2019, 9, 13, 9, 0)
-        due_date = calculate_due_date(submit_time, 0)
-        self.assertEqual(submit_time, due_date)
-
-    def test_turnaround_time_one_hour_due_date_is_submit_time_plus_one_hour(self):
-        submit_time = datetime(2019, 9, 13, 9, 0)
-        expected_due_date = datetime(2019, 9, 13, 10, 0)
-        due_date = calculate_due_date(submit_time, 1)
-        self.assertEqual(expected_due_date, due_date)
-
-    def test_turnaround_time_zero_hours_submit_time_is_5_pm_duedate_next_day_9_am(self):
-        submit_time = datetime(2019, 9, 12, 17, 0)
-        expected_due_date = datetime(2019, 9, 13, 9, 0)
-        due_date = calculate_due_date(submit_time, 0)
-        self.assertEqual(expected_due_date, due_date)
-
-    def test_turnaround_time_one_hour_working_time_left_is_less_due_date_next_day(self):
-        submit_time = datetime(2019, 9, 12, 16, 38)
-        expected_due_date = datetime(2019, 9, 13, 9, 38)
-        due_date = calculate_due_date(submit_time, 1)
-        self.assertEqual(expected_due_date, due_date)
+    @unpack
+    @data(
+        {
+            'test_case': 'Turnaround time is zero, due date is submit time',
+            'submit_time': datetime(2019, 9, 13, 9, 0),
+            'turnaround_time': 0,
+            'expected_due_date': datetime(2019, 9, 13, 9, 0)
+        },
+        {
+            'test_case': 'Turnaround time is zero, due date is submit time',
+            'submit_time': datetime(2019, 9, 13, 9, 0),
+            'turnaround_time': 1,
+            'expected_due_date': datetime(2019, 9, 13, 10, 0)
+        },
+        {
+            'test_case': 'Turnaround time is zero, due date is submit time',
+            'submit_time': datetime(2019, 9, 12, 17, 0),
+            'turnaround_time': 0,
+            'expected_due_date': datetime(2019, 9, 13, 9, 0)
+        },
+        {
+            'test_case': 'Turnaround time is zero, due date is submit time',
+            'submit_time': datetime(2019, 9, 12, 16, 38),
+            'turnaround_time': 1,
+            'expected_due_date': datetime(2019, 9, 13, 9, 38)
+        }
+    )
+    def test_due_date_calculation(self, test_case, submit_time, turnaround_time, expected_due_date):
+        due_date = calculate_due_date(submit_time, turnaround_time)
+        self.assertEqual(expected_due_date, due_date, msg=test_case)
